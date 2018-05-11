66545915026bed8f10e3be5b140cce6118956188
Sébastien Saunier <seb@saunier.me>
2018-05-11 10:14:32 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-// TODO: Build an awesome garage!
+import { addCar, fetchAndDisplayCars } from './car';
+import { focusOnFirstInput, prepareNewCarForm } from './form';
+
+const GARAGE_SLUG = "lewagon-is-awesome";
+
+fetchAndDisplayCars(GARAGE_SLUG);
+prepareNewCarForm(GARAGE_SLUG);
